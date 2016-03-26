221b444fce1a3978e9ef8d7212139279b800bbd2
Yusuf Eka <yusufblegoh@gmail.com>
2016-03-26 20:52:30 +0700

    adding main class

---
+++ src/mvc/Mvc.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc;
+
+import controller.CalcController;
+import model.CalcModel;
+import view.CalcView;
+
+/**
+ *
+ * @author blegoh
+ */
+public class Mvc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        CalcModel theModel = new CalcModel();
+        CalcView theView = new CalcView();
+        CalcController theController = new CalcController(theModel, theView);
+        theView.setVisible(true);
+    }
+    
+}
